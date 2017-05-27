92f290ef66ac634c5cb81b2fdae755ff6bfced34
Maro <grafika.marekduda@gmail.com>
2017-05-27 22:07:16 +0200

    Project 8.4

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// alert('test')
+
+var name = prompt('Enter your name');
+alert('hello, ' + name);
+console.log('hello, ' + name);
+
+/*
+ W tym zadaniu mam dodać tylko komentarz?
+*/
---
+++ readme.md
@@ -0,0 +1,1 @@
+#8.4 Trudne początki
